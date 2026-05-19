de353fe99e5d78f460db67d621b1da337009ad4c
IlariA-16 <ilariataddei@hotmail.com>
2026-05-19 09:48:56 +0200

    feat: completati ed estetizzati componenti header e footer

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CartService } from '../../services/cart';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrls: ['./header.css']
 })
-export class Header {}
+export class Header { // Controlla se la tua classe si chiama Header o HeaderComponent
+  constructor(private cartService: CartService) {}
+
+  // Funzione che l'HTML userà per mostrare quanti oggetti ci sono nel carrello
+  getCartCount(): number {
+    return this.cartService.getItems().length;
+  }
+}
