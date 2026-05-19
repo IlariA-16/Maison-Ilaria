3baa9938a9c04ddce44430ddf8445632c82ce104
IlariA-16 <ilariataddei@hotmail.com>
2026-05-19 09:54:23 +0200

    feat: implementate pagine Home e Collezione con design editoriale d'alta moda

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterModule], // Serve per far funzionare il bottone che porta allo shop
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
 export class Home {}
